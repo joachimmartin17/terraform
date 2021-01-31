9118e701daa1606d58959ced3d2de6862000d221
Joachim Martin <joachimmartin07@gmail.com>
2021-01-31 15:37:13 -0500

    added user data

--- main.tf
+++ main.tf
@@ -7,6 +7,12 @@ secret_key = "AYn7SIq8kNQLo3ob9NufQ6qNv5PO4UvnYqR8MEpd"
 resource "aws_instance" "example" {
   ami          = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
     "Name" = "terraform-examplebenji"

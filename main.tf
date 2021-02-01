122d88e8f157ddc21c48d5070a6e2953a6229a5a
Joachim Martin <joachimmartin07@gmail.com>
2021-02-01 18:42:52 -0500

    added variable and ouput propert to main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,43 @@
 provider "aws" {
   region = "us-east-2"
-access_key = "AKIA4VBIC52MMGL6YPOG"
-secret_key = "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"
+  access_key = "AKIAXA5UNWV5IPHEV3NP"
+  secret_key = "6K/sAzTZHWZE6pYv27J8SmoEvXZt4Ks8mD/hhfm5"
 
 }
+
 resource "aws_instance" "example" {
-  ami          = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ "aws_secuirty_group.instance.id" ]
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
-resource "awssecurity_groups" "instance" {
-  name = "terraform-example-instance"
-  from_port = 8080
-  to_port = 8080
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  
+
   tags = {
-    "Name" = "terraform-examplebenji"
+    Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "terraform-example-instance"
+}
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP of the Instance"
 }
